21ad10eceacc032198910c6c1e415e0a698f2eec
Carlos E. Suito Schacht <csuito@iPhonedeLeonor.localdomain>
2020-07-01 18:15:54 +0200

    fixing bestsellers return value

--- server/resolvers/bestsellers.js
+++ server/resolvers/bestsellers.js
@@ -13,7 +13,7 @@ const getBestsellers = async (_, { url, page = 1 }) => {
   }
 
   try {
-    const { data: { bestsellers, pagination } } = await client.get("/", { params })
+    const { data: { bestsellers: { bestsellers, pagination } } } = await client.get("/", { params })
     return { bestsellers, pagination }
   } catch (e) {
     return { success: false }
