1337d79036b83ca606babd435e88103245984c4a
HOllarves <henryollh205@gmail.com>
2020-08-25 11:24:32 +0200

    Updating tests

---
+++ test/banners.test.js
@@ -0,0 +1,77 @@
+// https://easygraphql.com/docs/easygraphql-tester/usage/#mocking-queries-and-mutations
+const GraphQLTester = require("easygraphql-tester")
+const { gql } = require("apollo-server-express")
+const { expect } = require("chai")
+
+const typeDefs = require("../server/typedefs")
+const resolvers = require("../server/resolvers")
+
+describe("test graphql DEPARTMENTS queries", () => {
+  const bannersQuery = gql`
+    query getBanners {
+      banners {
+        text
+        type
+        image
+      }
+    }
+  `
+
+  let tester
+  before(() => {
+    tester = new GraphQLTester(typeDefs, resolvers)
+  })
+
+  it("should fail on invalid banners query", async () => {
+    const invalidQuery = gql`
+      query getBanners {
+        banners {
+          text
+          type
+          image
+          invalid_field
+        }
+      }
+    `
+    tester.test(false, { query: invalidQuery })
+  })
+
+  it("should pass on valid banners query", () => {
+    tester.test(true, { query: bannersQuery })
+  })
+
+  it("should return mocked fields on banners query", () => {
+    const fixture = {
+      data: {
+        banners: [
+          {
+            text: "Banner text!",
+            type: "referral",
+            image: "https://image.com/?w=400&h=200"
+          },
+          {
+            text: "Banner text!",
+            type: "referral",
+            image: "https://image.com/?w=400&h=200"
+          },
+          {
+            text: "Banner text!",
+            type: "referral",
+            image: "https://image.com/?w=400&h=200"
+          }
+        ]
+      }
+    }
+    const { data: { banners } } = tester.mock({ query: bannersQuery, fixture })
+
+    expect(banners).to.be.a("array")
+
+    banners.forEach((p, i) => {
+      const og = fixture.data.banners[i]
+      expect(p.text).to.be.a("string").and.be.equal(og.text)
+      expect(p.type).to.be.a("string").and.be.equal(og.type)
+      expect(p.image).to.be.a("string").and.be.equal(og.image)
+    })
+  })
+
+})
---
+++ test/products.test.js
@@ -0,0 +1,83 @@
+// https://easygraphql.com/docs/easygraphql-tester/usage/#mocking-queries-and-mutations
+const GraphQLTester = require("easygraphql-tester")
+const { gql } = require("apollo-server-express")
+const { expect } = require("chai")
+
+const typeDefs = require("../server/typedefs")
+const resolvers = require("../server/resolvers")
+
+describe("test graphql PRODUCTS queries", () => {
+  const productsQuery = gql`
+    query getProducts($department: String! $category: String) {
+      products(department: $department category: $category) {
+        asin
+        title
+        brand
+      }
+    }
+  `
+
+
+  const testVariables = {
+    department: "Bebe",
+    category: "Regalos para recien nacidos"
+  }
+
+  let tester
+  before(() => {
+    tester = new GraphQLTester(typeDefs, resolvers)
+  })
+
+  it("should fail on invalid product query", async () => {
+    const invalidQuery = gql`
+      query getProducts($department: String! $category: String) {
+        products(department: $department category: $category) {
+          asin
+          title
+          brand
+          invalid_field
+        }
+      }
+    `
+    tester.test(false, { query: invalidQuery, variables: testVariables })
+  })
+
+  it("should pass on valid product query", () => {
+    tester.test(true, { query: productsQuery, variables: testVariables })
+  })
+
+  it("should return mocked fields on product query", () => {
+    const fixture = {
+      data: {
+        products: [
+          {
+            asin: "XSSFDA",
+            title: "Some title",
+            brand: "Some brand"
+          },
+          {
+            asin: "XSSFDA",
+            title: "Some title",
+            brand: "Some brand"
+          },
+          {
+            asin: "XSSFDA",
+            title: "Some title",
+            brand: "Some brand"
+          }
+        ]
+      }
+    }
+    const { data: { products } } = tester.mock({ query: productsQuery, fixture, variables: testVariables })
+
+    expect(products).to.be.a("array")
+
+    products.forEach((p, i) => {
+      const og = fixture.data.products[i]
+      expect(p.asin).to.be.a("string").and.be.equal(og.asin)
+      expect(p.title).to.be.a("string").and.be.equal(og.title)
+      expect(p.brand).to.be.a("string").and.be.equal(og.brand)
+    })
+  })
+
+})
